e97e739d1118681ce313c8ff2a1d3fab3ebf9c8b
zefr0x <zer0-x.7ty50@aleeas.com>
2024-03-27 07:47:38 +0300

    gui(adminPage): add base components

--- app/src/main/java/carsdealership/AdminPage.java
+++ app/src/main/java/carsdealership/AdminPage.java
@@ -1,6 +1,11 @@
 package carsdealership;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import javax.swing.JPanel;
+import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 
@@ -10,12 +15,110 @@ class AdminPage extends JPanel {
     AdminPage(MainWindow parent) {
         this.parent = parent;
 
-        this.add(new JLabel("Admin Page"));
+        this.setLayout(new BorderLayout());
+
+        JPanel headerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        this.add(headerPanel, BorderLayout.NORTH);
+
+        Box bodyBox = Box.createVerticalBox();
+        bodyBox.setAlignmentX(Component.LEFT_ALIGNMENT);
+        this.add(bodyBox);
+
+        JPanel footerPanel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+        this.add(footerPanel, BorderLayout.SOUTH);
+
+        // Page Title
+        JLabel pageTitle = new JLabel("Admin DashBoard");
+        pageTitle.setFont(new Font("Serf", Font.BOLD, 20));
+        headerPanel.add(pageTitle);
+
+        // Users managment section
+        bodyBox.add(Box.createVerticalStrut(20));
+        bodyBox.add(new JLabel("System Users Managment"));
+        Box usersManagmentBox = Box.createHorizontalBox();
+        usersManagmentBox.setAlignmentX(Component.LEFT_ALIGNMENT);
+        bodyBox.add(usersManagmentBox);
+
+        JButton newManagerButton = new JButton("Add New Manager");
+        newManagerButton.addActionListener(
+                e -> {
+
+                });
+        usersManagmentBox.add(newManagerButton);
+
+        JButton newSellerButton = new JButton("Add New SalesMan Account");
+        newSellerButton.addActionListener(
+                e -> {
+
+                });
+        usersManagmentBox.add(newSellerButton);
+
+        // Products managment section
+        bodyBox.add(Box.createVerticalStrut(20));
+        bodyBox.add(new JLabel("Products Managment"));
+        Box productsManagmentBox = Box.createHorizontalBox();
+        productsManagmentBox.setAlignmentX(Component.LEFT_ALIGNMENT);
+        bodyBox.add(productsManagmentBox);
+
+        JButton newItemButton = new JButton("Create New Product");
+        newItemButton.addActionListener(
+                e -> {
+
+                });
+        productsManagmentBox.add(newItemButton);
+
+        JButton updateItemButton = new JButton("Update Product Quantity");
+        updateItemButton.addActionListener(
+                e -> {
+
+                });
+        productsManagmentBox.add(updateItemButton);
+
+        JButton addDiscountButton = new JButton("Apply Discount to Product");
+        addDiscountButton.addActionListener(
+                e -> {
+
+                });
+        productsManagmentBox.add(addDiscountButton);
+
+        // Reports section
+        bodyBox.add(Box.createVerticalStrut(20));
+        bodyBox.add(new JLabel("System Reports"));
+        Box reportsBox = Box.createHorizontalBox();
+        reportsBox.setAlignmentX(Component.LEFT_ALIGNMENT);
+        bodyBox.add(reportsBox);
+
+        JButton salesReportButton = new JButton("Sales Report");
+        salesReportButton.addActionListener(
+                e -> {
+
+                });
+        reportsBox.add(salesReportButton);
+
+        JButton goodsReportButton = new JButton("Products Report");
+        goodsReportButton.addActionListener(
+                e -> {
+
+                });
+        reportsBox.add(goodsReportButton);
+
+        JButton usersReportButton = new JButton("System Users Report");
+        usersReportButton.addActionListener(
+                e -> {
+
+                });
+        reportsBox.add(usersReportButton);
+
+        JButton changePasswordButton = new JButton("Change My Password");
+        changePasswordButton.addActionListener(
+                e -> {
+
+                });
+        footerPanel.add(changePasswordButton);
 
         JButton logoutButton = new JButton("Logout");
         logoutButton.addActionListener(
                 e -> parent.container.remove(this));
-        this.add(logoutButton);
-
+        footerPanel.add(logoutButton);
     }
 }
